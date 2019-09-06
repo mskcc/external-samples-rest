ea01079e30da54e3a497cb3837eb2f7d15c4fed6
imprezka88 <imprezka88@gmail.com>
2019-09-06 16:39:39 -0400

    [external-samples-rest-4] Search by dmp and cmo patient id

--- src/main/java/org/mskcc/igo/pi/external/rest/sample/ExternalSampleGateway.java
+++ src/main/java/org/mskcc/igo/pi/external/rest/sample/ExternalSampleGateway.java
@@ -2,11 +2,16 @@ package org.mskcc.igo.pi.external.rest.sample;
 
 import org.mskcc.domain.external.ExternalSample;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface ExternalSampleGateway {
     Optional<ExternalSample> getSample(String id);
+
+    Collection<ExternalSample> getSamplesByCmoPatientId(String patientCmoId);
+
+    Collection<ExternalSample> getSamplesByDmpPatientId(String patientDmpId);
 
     void addSample(ExternalSample externalSample);
 
--- src/main/java/org/mskcc/igo/pi/external/rest/sample/ExternalSampleRepository.java
+++ src/main/java/org/mskcc/igo/pi/external/rest/sample/ExternalSampleRepository.java
@@ -4,7 +4,13 @@ import org.mskcc.igo.pi.external.jpa.entity.ExternalSampleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+
 @Component
 public interface ExternalSampleRepository extends JpaRepository<ExternalSampleEntity, Long> {
     ExternalSampleEntity findByExternalId(String externalId);
+
+    Collection<ExternalSampleEntity> findByPatientCmoId(String patientCmoId);
+
+    Collection<ExternalSampleEntity> findByPatientExternalId(String patientDmpId);
 }
